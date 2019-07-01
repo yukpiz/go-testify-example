fa1597a91792eff795dfa2a219dcbf7f4fd148a5
yukpiz <yukpiz@gmail.com>
2019-07-01 13:28:43 +0900

    updated

--- test_test.go
+++ test_test.go
@@ -1,9 +1,12 @@
 package test
 
 import (
+	"log"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/xerrors"
 )
 
 type User struct {
@@ -26,7 +29,14 @@ func Test_Example1(t *testing.T) {
 
 	u3 := &User{
 		ID:   1,
-		Name: "yukpiz2",
+		Name: "yukpiz",
 	}
 	assert.Equal(t, u2, u3)
+}
+
+func Test_Example2(t *testing.T) {
+	x1 := xerrors.New("aaaaa")
+
+	log.Println(reflect.TypeOf(x1))
+	assert.EqualError(t, x1, "aaaaa")
 }
